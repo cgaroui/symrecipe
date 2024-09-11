3407bf8100b158e46665a27ea0367baf00b859d5
chaima garoui <chaimagaroui2@gmail.com>
2024-09-11 17:17:14 +0200

    ajout pagination avec knppaginatorbundle ok

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'app_ingredient')]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator,  Request $request): Response
+    {
+
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit par page*/
+        );
+      
+
+        return $this->render('pages/ingredient/index.html.twig', [
+                'ingredients' => $ingredients
+        ]);
+    }
+}
