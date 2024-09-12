c2d4fb4452b3a68868a0d4d7a9f91bd5b595ae67
chaima garoui <chaimagaroui2@gmail.com>
2024-09-12 16:29:55 +0200

    ajout : opérations update et delete ok (CRUD fini)

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -36,7 +36,7 @@ class IngredientController extends AbstractController
         ]);
     }
 
-    #[Route('/ingredient/nouveau', name: 'ingredient.new', methods: ['GET', 'POST'])]
+    #[Route('/ingredient/nouveau', name: 'ingredient_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $manager): Response
     {
         $ingredient = new Ingredient();
@@ -57,4 +57,53 @@ class IngredientController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    
+   
+    #[Route('/ingredient/edit/{id}', name: 'ingredient_edit', methods: ['GET', 'POST'])]
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager) : Response
+    {
+
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre ingrédient a été modifié avec succées !');
+
+            // Redirection après le succès
+            return $this->redirectToRoute('app_ingredient');
+        }
+
+    
+
+        return $this->render('pages/ingredient/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/ingredient/supprimer/{id}', name: 'ingredient_delete', methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager, Ingredient $ingredient): Response
+    {
+        if (!$ingredient) {
+            $this->addFlash(
+                'error',
+                'L\'ingrédient en question n\'a pas été trouvé !'
+            );
+            return $this->redirectToRoute('app_ingredient');
+        }
+    
+        $manager->remove($ingredient);
+        $manager->flush();
+    
+        $this->addFlash(
+            'success',
+            'Votre ingrédient a été supprimé avec succès !'
+        );
+    
+        return $this->redirectToRoute('app_ingredient');
+    }
+    
 }
